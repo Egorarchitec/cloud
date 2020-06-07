c94993ca472f8ebf47231a6da1b084ca9beab676
Egorarchitec <66465822+Egorarchitec@users.noreply.github.com>
2020-06-07 16:26:06 +0200

    Update sliceupload.php

--- sliceupload.php
+++ sliceupload.php
@@ -5,36 +5,42 @@ require_once('clean_folder.php');
 require_once('writable_check.php');
 
 session_start();
-$tmp_dir = setting::$writepath . $_POST['name'] . "_" . $_SESSION['username'] . '/'; //creates unique file dependent folder to upload chunks
-mkdir($tmp_dir);
+$tmp_dir = setting::$writepath . $_POST['name'] . "_" . $_SESSION['username'] . '/'; //creates unique file dependent folder to upload chunksif (isset($_FILES['file'])) {
+    if (!file_exists($tmp_dir)) {
+        mkdir($tmp_dir);
+    }
 
-$tmp_name = $_FILES['file']['tmp_name'];
-$file_name = $_POST['name'];
-$chunkname = $_POST['chunkname'];
-$filecount = $_POST['chunkammount'];
-$chunk_target = $tmp_dir . $chunkname;
-$file_target = setting::$writepath . $file_name;
-move_uploaded_file($tmp_name, $chunk_target);
+    $tmp_name = $_FILES['file']['tmp_name'];
+    $file_name = $_POST['name'];
+    $chunkname = $_POST['chunkname'];
+    $filecount = $_POST['chunkammount'];
+    $chunk_target = $tmp_dir . $chunkname;
+    $file_target = setting::$writepath . $file_name;
 
-$chunkname = explode('_', $chunkname);
-$chunkindex = end($chunkname);
-$index = 0;
+    $chunkname = explode('_', $chunkname);
+    $chunkindex = end($chunkname);
 
-if ($chunkindex == 0) {
-    while ($index <= $filecount) {
-    
-        $file_name_i = $tmp_dir . $file_name . '_' . $index;
-        $range = filesize($file_name_i);
-        $tmpfile = fopen($file_name_i, 'rb');
-        $buffer = fread($tmpfile, $range);
-        fclose($tmpfile);
-        unlink($file_name_i);
+    move_uploaded_file($tmp_name, $chunk_target);
 
-        $mergedhandler = fopen($file_target, 'ab');
-        $merged_file = fwrite($mergedhandler, $buffer);
-        fclose($mergedhandler);
-        unlink($chunk_target);
 
-        $index++;
+    $index = 0;
+
+    if ($chunkindex == 0) {
+        while ($index <= $filecount - 1) {
+
+            $file_name_i = $tmp_dir . $file_name . '_' . $index;
+            $range = filesize($file_name_i);
+            $tmpfile = fopen($file_name_i, 'rb');
+            $buffer = fread($tmpfile, $range);
+            fclose($tmpfile);
+            unlink($file_name_i);
+
+            $mergedhandler = fopen($file_target, 'ab');
+            $merged_file = fwrite($mergedhandler, $buffer);
+            fclose($mergedhandler);
+
+            $index++;
+        }
+        rmdir($tmp_dir);
     }
 }

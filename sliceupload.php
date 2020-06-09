1cb337a1ee4473121d8830658855dd85f18c8476
Egorarchitec <66465822+Egorarchitec@users.noreply.github.com>
2020-06-09 23:14:33 +0200

    Add files via upload

--- sliceupload.php
+++ sliceupload.php
@@ -3,44 +3,24 @@
 require_once('settings.php');
 require_once('clean_folder.php');
 require_once('writable_check.php');
+require_once('readfolder.php');
+require_once('saveuploaded.php');
 
 session_start();
-$tmp_dir = setting::$writepath . $_POST['name'] . "_" . $_SESSION['username'] . '/'; //creates unique file dependent folder to upload chunksif (isset($_FILES['file'])) {
-    if (!file_exists($tmp_dir)) {
-        mkdir($tmp_dir);
-    }
+
+
+if (isset($_POST['name'])) {
 
     $tmp_name = $_FILES['file']['tmp_name'];
     $file_name = $_POST['name'];
+    $index = $_POST['index'];
+    $targetsize = $_POST['size'];
+    $file_ext = explode('.', $file_name);
+    $file_ext = end($file_ext);
     $chunkname = $_POST['chunkname'];
     $filecount = $_POST['chunkammount'];
-    $chunk_target = $tmp_dir . $chunkname;
-    $file_target = setting::$writepath . $file_name;
 
-    $chunkname = explode('_', $chunkname);
-    $chunkindex = end($chunkname);
+    $filetarget = setting::$writepath . $file_name;
 
-    move_uploaded_file($tmp_name, $chunk_target);
-
-
-    $index = 0;
-
-    if ($chunkindex == 0) {
-        while ($index <= $filecount - 1) {
-
-            $file_name_i = $tmp_dir . $file_name . '_' . $index;
-            $range = filesize($file_name_i);
-            $tmpfile = fopen($file_name_i, 'rb');
-            $buffer = fread($tmpfile, $range);
-            fclose($tmpfile);
-            unlink($file_name_i);
-
-            $mergedhandler = fopen($file_target, 'ab');
-            $merged_file = fwrite($mergedhandler, $buffer);
-            fclose($mergedhandler);
-
-            $index++;
-        }
-        rmdir($tmp_dir);
-    }
+    saveuploaded($tmp_name, $filetarget, $index, $targetsize, $filecount);
 }
